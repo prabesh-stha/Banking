fbd23b70e068978dbb6a2a1e85ee827b02789e14
Prabesh Shrestha <shresthaprabesh155@gmail.com>
2025-02-05 19:19:01 +0545

    Initial Commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Prabesh Shrestha on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
